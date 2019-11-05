44bd139f4aff024097906f0ca2d36910fa385f6a
Whiskarek <andrei5709978@gmail.com>
2019-11-05 22:21:04 +0300

    Осталось вычислить выражение

--- app/src/main/java/by/whiskarek/calculator/BasicModeFragment.java
+++ app/src/main/java/by/whiskarek/calculator/BasicModeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,6 +15,9 @@ public class BasicModeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.basic, container, false);
+        View view = inflater.inflate(R.layout.basic, container, false);
+        Button del = view.findViewById(R.id.btn_del);
+        del.setOnLongClickListener((MainActivity) getActivity());
+        return view;
     }
 }
--- app/src/main/java/by/whiskarek/calculator/MainActivity.java
+++ app/src/main/java/by/whiskarek/calculator/MainActivity.java
@@ -7,17 +7,23 @@ import androidx.viewpager.widget.ViewPager;
 
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnLongClickListener {
+
+    private EditText screen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         onCreateFlavor();
+        screen = findViewById(R.id.screen);
     }
 
     private void onCreateFlavor() {
@@ -48,5 +54,43 @@ public class MainActivity extends AppCompatActivity {
             FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
             fragmentTransaction.add(R.id.mode_basic, calculatorModes.get(0)).add(R.id.mode_scientific, calculatorModes.get(1)).commit();
         }
+    }
+
+    public void onKeyPress(View view) {
+        switch (view.getId()) {
+            case R.id.btn_eq:
+                onEquals();
+                break;
+            case R.id.btn_del:
+                onDelete();
+                break;
+            case R.id.btn_cos:
+            case R.id.btn_ln:
+            case R.id.btn_log:
+            case R.id.btn_sin:
+            case R.id.btn_tan:
+                screen.append(((Button) view).getText() + "(");
+                break;
+            default:
+                screen.setText(screen.getText().append(((Button) view).getText()));
+                break;
+        }
+    }
+
+    private void onDelete() {
+        int text_size = screen.getText().length();
+        if (text_size > 0)
+            screen.setText(screen.getText().delete(text_size - 1, text_size));
+    }
+
+    private void onEquals() {
+
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+        if (v.getId() == R.id.btn_del)
+            screen.setText("");
+        return true;
     }
 }
